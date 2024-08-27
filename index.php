f60449b3bb2ec1e1b6cfdc9a33b4b324aab65fbf
nikhil <nikhilyagik2004@gmail.com>
2024-08-27 16:18:12 +0530

    changes

--- index.php
+++ index.php
@@ -95,28 +95,7 @@
         </div>
     </div>
 
-    <nav class="navbar">
-        <div class="container-fluid">
-            
-            <a class="navbar-brand" href="#" id="title"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-list-columns-reverse" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 .5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10A.5.5 0 0 1 4 .5m-4 2A.5.5 0 0 1 .5 2h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 4h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 6h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 8h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
-</svg>Notify</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="search-container">
-
-    <input type="text" id="search-input"  placeholder="Search notes...">
-    <button id="search-button"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-</svg></button>
-            
-        </div>
-        
-</div>
-    </nav>
+   <?php require "nav.php"?>
     <?php
 if($alert){
   echo'<div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -139,16 +118,16 @@ if($delete){
 ?>
 
 
-<div class="sort-buttons">
-    <button id="sort-name">Name ↕</button>
-    <button id="sort-description">Description ↕</button>
-    <button id="sort-time">Time ↕</button>
-</div>
+    <div class="sort-buttons">
+        <button id="sort-name">Name ↕</button>
+        <button id="sort-description">Description ↕</button>
+        <button id="sort-time">Time ↕</button>
+    </div>
 
-<div id="notes-container">
-    
+    <div id="notes-container">
 
-    <?php
+
+        <?php
     $sql="SELECT * FROM `notes`";
     $result=mysqli_query($conn,$sql);
     $sno=1;
@@ -170,18 +149,20 @@ if($delete){
        ';
        $sno+=1;
     }
-    ?> 
-   
-</div>
+    ?>
+
+    </div>
     <div class="add">
 
-      <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="orange" class="bi bi-plus-circle-fill " viewBox="0 0 16 16" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="orange" class="bi bi-plus-circle-fill "
+            viewBox="0 0 16 16" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <path
+                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
         </svg>
     </div>
-   
-   
- <!-- </button>  -->
+
+
+
 
 
     <!-- Modal -->
@@ -212,17 +193,17 @@ if($delete){
                             <button type="submit" class="btn btn-primary">Add</button>
                     </form>
                 </div>
-                <!-- </div> -->
-                <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div> -->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+               
             </div>
         </div>
     </div>
-    
+    <!-- </div> -->
 
-    
+
+
 
 
     <!-- Optional JavaScript; choose one of the two! -->
@@ -234,7 +215,7 @@ if($delete){
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <script src="//cdn.datatables.net/2.1.3/js/dataTables.min.js"></script>
-    
+
     <script src="script.js"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
@@ -243,5 +224,4 @@ if($delete){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
 </body>
-
 </html>
---
+++ nav.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+if( isset($_SESSION['loggedin'])  && $_SESSION['loggedin']==true){
+  $loggedin=true;
+}else{
+  $loggedin=false;
+}
+
+echo'<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Notes </title>
+    <link rel="stylesheet" href="//cdn.datatables.net/2.1.3/css/dataTables.dataTables.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<nav class="navbar">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#" id="title"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+                    fill="white" class="bi bi-list-columns-reverse" viewBox="0 0 16 16" class="openbtn" onclick="openNav()">
+                    <path fill-rule="evenodd"
+                        d="M0 .5A.5.5 0 0 1 .5 0h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 .5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10A.5.5 0 0 1 4 .5m-4 2A.5.5 0 0 1 .5 2h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 4h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 6h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 8h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
+                </svg>Notify</a>
+               
+    <div id="sideNav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="index.php">Home</a>';
+        if(!$loggedin){
+          echo'
+          <a href="login.php">login</a>
+          <a href="signup.php">signup</a>';
+        }if($loggedin){
+          echo'
+          <a href="logout.php">logout</a>';
+        }
+        echo'
+    </div>
+
+  
+            
+            <div class="search-container">
+
+                <input type="text" id="search-input" placeholder="Search notes...">
+                <button id="search-button"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white"
+                        class="bi bi-search" viewBox="0 0 16 16">
+                        <path
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                    </svg></button>
+
+            </div>
+
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <script src="//cdn.datatables.net/2.1.3/js/dataTables.min.js"></script>
+
+    <script src="script.js"></script>
+    <script>
+      function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.querySelector(".main-content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.querySelector(".main-content").style.marginLeft = "0";
+}
+
+    </script>
+
+   
+</body>
+</html>';
+?>
